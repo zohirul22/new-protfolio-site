385acc31c32835ec3a9c5a0cc3dbb724dd478565
zohirul22 <shahin57islam@gmail.com>
2023-07-09 15:36:44 +0600

    services and testominlas section

--- asserts/js/index.js
+++ asserts/js/index.js
@@ -1,25 +1,21 @@
-const navbarMenu = document.getElementById("menu");
-const burgerMenu = document.getElementById("burger");
-const bgOverlay = document.querySelector(".overlay");
+$('.owl-carousel').owlCarousel({
 
-// Show Menu when Click the Burger
-// Hide Menu when Click the Overlay
-if (burgerMenu && navbarMenu && bgOverlay) {
-	burgerMenu.addEventListener("click", () => {
-		navbarMenu.classList.toggle("is-active");
-		bgOverlay.classList.toggle("is-active");
-	});
-
-	bgOverlay.addEventListener("click", () => {
-		navbarMenu.classList.toggle("is-active");
-		bgOverlay.classList.toggle("is-active");
-	});
-}
-
-// Hide Menu when Click the Links
-document.querySelectorAll(".menu-link").forEach((link) => {
-	link.addEventListener("click", () => {
-		navbarMenu.classList.remove("is-active");
-		bgOverlay.classList.remove("is-active");
-	});
-});
+    margin: 30,
+    nav: true,
+   
+    dots: false,
+    loop: true,
+    navText:["<i class='fa fa-arrow-left' ></i>", 
+    "<i class='fa fa-arrow-right' ></i>"],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
