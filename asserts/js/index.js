f13dd1a161b500dbeff09dfc51495d37dd481803
zohirul22 <shahin57islam@gmail.com>
2023-07-10 10:12:32 +0600

    responsive code

--- asserts/js/index.js
+++ asserts/js/index.js
@@ -2,11 +2,11 @@ $('.owl-carousel').owlCarousel({
 
     margin: 30,
     nav: true,
-   
+
     dots: false,
     loop: true,
-    navText:["<i class='fa fa-arrow-left' ></i>", 
-    "<i class='fa fa-arrow-right' ></i>"],
+    navText: ["<i class='fa fa-arrow-left' ></i>",
+        "<i class='fa fa-arrow-right' ></i>"],
     responsive: {
         0: {
             items: 1
@@ -19,3 +19,13 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+// header js
+let navMenu = document.querySelector('#menu-icon');
+let toggleMenu = document.querySelector('.navbar');
+
+navMenu.onclick = () => {
+    navMenu.classList.toggle('bx-x');
+    toggleMenu.classList.toggle('open');
+
+}
